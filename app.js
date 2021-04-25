fe29f97e4ea39e9333010c1765d747d0b2e40eea
d-bizari <daniel.bizari@mercadolibre.com>
2021-04-25 14:44:42 -0300

    add body parser dependency and addde handler for testing e2e with mobile

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Constants
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
-
+ const PORT = process.env.PORT || 8080;
 // App
 const app = express();
 
-app.use(express.json()) // for parsing application/json
-app.use(               // for parsing application/x-www-form-urlencoded
-  express.urlencoded({
-    extended: true
-  })
-)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 function nameHandler(req, res) {
     console.log("incoming request...")
@@ -48,8 +46,32 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/full-name/:id', nameHandler);
-app.post('/full-name/:id', nameHandler);
+app.get('/full-name/:id', (req, res) => {
+    console.log(req.params)
+    console.log(req.body)
+    console.log(req.query)
+
+    let name = ""
+    let lastname = ""
+    let id = 0
+    try {
+        name = req.query["name"]
+        lastname = req.body["last_name"]
+        id = req.params["id"]
+    } catch (e) {
+        console.log(e);
+        console.error(e.message)
+
+        res.status(500).send('some field is misssing!');
+        return
+    }
+
+    let respBody = {
+        "full_name": "Hello " + id.toString() + " " + name + " " + lastname
+    }
+
+    res.status(200).json(respBody);
+});
 
 app.listen(PORT, () =>
     console.log(`server is up`)
