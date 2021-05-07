82b84ec7892e5cc0b20626d0d731ffd77f9a173a
Daniela Carrero <danielacarreror@hotmail.com>
2021-05-07 01:59:47 -0300

    add mongodb connection & router structure

---
+++ src/utils/responses.js
@@ -0,0 +1,15 @@
+exports.notFoundResponse = function (res, msg) {
+    var data = {
+        status: 404,
+        message: msg,
+    };
+    return res.status(404).json(data);
+};
+
+exports.unauthorizedResponse = function (res, msg) {
+    var data = {
+        status: 401,
+        message: msg,
+    };
+    return res.status(401).json(data);
+};
