fe2a04515486cae8fb20eb6e3127d6985022f0ae
d-bizari <daniel.bizari@mercadolibre.com>
2021-05-09 22:24:00 -0300

    add linter to project and to ci workflow

--- app.js
+++ app.js
@@ -4,75 +4,47 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Constants
- const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 // App
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-
-function nameHandler(req, res) {
-    console.log("incoming request...")
-    console.log(req.params)
-    console.log(req.body)
-    console.log(req.query)
-
-    let name = ""
-    let lastname = ""
-    let id = 0
-    try {
-        name = req.query["name"]
-        lastname = req.body["last_name"]
-        id = req.params["id"]
-    } catch (e) {
-        console.log(e);
-        console.error(e.message)
-
-        res.status(500).send('Name or lastname is misssing!');
-        return
-    }
-
-    let respBody = {
-        "full name": "Hello " + id.toString() + " " + name + " " + lastname
-    }
-
-    res.json(respBody);
-}
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+	res.send('Hello World');
 });
 
 app.get('/full-name/:id', (req, res) => {
-    console.log(req.params)
-    console.log(req.body)
-    console.log(req.query)
+	console.log(req.params);
+	console.log(req.body);
+	console.log(req.query);
 
-    let name = ""
-    let lastname = ""
-    let id = 0
-    try {
-        name = req.query["name"]
-        lastname = req.body["last_name"]
-        id = req.params["id"]
-    } catch (e) {
-        console.log(e);
-        console.error(e.message)
+	let name = '';
+	let lastname = '';
+	let id = 0;
+	try {
+		name = req.query['name'];
+		lastname = req.body['last_name'];
+		id = req.params['id'];
+	} catch (e) {
+		console.log(e);
+		console.error(e.message);
 
-        res.status(500).send('some field is misssing!');
-        return
-    }
+		res.status(500).send('some field is misssing!');
+		return;
+	}
 
-    let respBody = {
-        "full_name": "Hello " + id.toString() + " " + name + " " + lastname
-    }
+	let respBody = {
+		'full_name': 'Hello ' + id.toString() + ' ' + name + ' ' + lastname
+	};
 
-    res.status(200).json(respBody);
+	res.status(200).json(respBody);
 });
 
 app.listen(PORT, () =>
-    console.log(`server is up`)
+	console.log('server is up')
 );
