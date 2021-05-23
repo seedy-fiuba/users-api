2d1c4be11475da03f7ffe8461f25909499940fe6
d-bizari <daniel.bizari@mercadolibre.com>
2021-05-23 20:24:42 -0300

    first test

--- src/__tests__/AuthController.test.js
+++ src/__tests__/AuthController.test.js
@@ -1,6 +1,31 @@
+const supertest = require('supertest')
 
-describe('test', () => {
-	test('test1', async () => {
+
+describe('POST /user', () => {
+	beforeEach(() => {
+		// algo
+	})
+
+	afterAll(() => {
+		// algo
+	})
+
+	test('create user', async () => {
 		expect(true).toBe(true);
 	});
 });
+
+// describe('POST /api/project', () => {//
+// 	test("should create a new project", async () => {
+// 		let body = {name: "proyecto1", description: "proyecto  re copado"}
+//
+// 		const res = await request.post("/api/project").send(body)
+//
+//
+// 		expect(projectMockRepository.createProject.mock.calls.length).toBe(1)
+// 		expect(projectMockRepository.createProject.mock.calls[0][0]).toBe(body.name)
+// 		expect(projectMockRepository.createProject.mock.calls[0][1]).toBe(body.description)
+// 		expect(res.status).toBe(200)
+// 		expect(res.body.message).toBe('project added successfully')
+// 	})
+// })
