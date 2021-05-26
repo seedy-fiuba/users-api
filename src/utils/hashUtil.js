868768bd051a6a9de9b2fff02e4f736b6f4aac75
Daniela Carrero <danielacarreror@hotmail.com>
2021-05-26 17:35:05 -0300

    hash util

---
+++ src/utils/hashUtil.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcryptjs');
+
+exports.encrypt = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+exports.validatePasswords = async(p1, p2) => {
+    return await bcrypt.compare(p1, p2);
+}
